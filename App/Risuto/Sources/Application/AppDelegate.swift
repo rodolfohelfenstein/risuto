91b6c5a8e4a990465035c13ab0cf0b4c84e1e02e
Rodolfo Helfenstein Bulgam <rodolfo.helfenstein@gmail.com>
2022-02-21 00:02:05 -0300

    SETUP: Added App Project

---
+++ App/Risuto/Sources/Application/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Risuto
+//
+//  Created by Rodolfo Helfenstein on 20/02/22.
+//
+
+import UIKit
+
+typealias LaunchOptions = [UIApplication.LaunchOptionsKey: Any]
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: LaunchOptions?) -> Bool {
+        return true
+    }
+
+}
+
